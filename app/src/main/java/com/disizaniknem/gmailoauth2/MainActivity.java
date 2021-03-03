3809bd074ccf0916c71df0f7db8961a70bdd71fd
disizaniknem <disizaniknem@gmail.com>
2021-03-03 20:22:12 +0100

    Layout

--- app/src/main/java/com/disizaniknem/gmailoauth2/MainActivity.java
+++ app/src/main/java/com/disizaniknem/gmailoauth2/MainActivity.java
@@ -1,14 +1,16 @@
 package com.disizaniknem.gmailoauth2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
